241be5aa2a284e870b9c6f1a3678da21c059876e
Ezequiel Leonardo Castaño <elcg@gmx.com>
2021-01-29 12:01:57 -0300

    Módulo 3: Solucionando Errores en Plantillas

--- publishconf.py
+++ publishconf.py
@@ -13,8 +13,6 @@ from pelicanconf import *
 SITEURL = 'https://elcg.github.io'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
